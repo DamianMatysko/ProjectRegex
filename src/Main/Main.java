88681aa01955a93624dbc76aad989ae4dc1ee7fd
FireflySK <mada11@azet.sk>
2020-02-13 12:15:39 +0100

    add

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,34 +2,44 @@ package Main;
 
 //import java.util.ArrayList;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import sk.itsovy.data.DataIP;
-
+import sk.itsovy.data.Date;
+import sk.itsovy.data.MacAddress;
+import sk.itsovy.data.PersonalID;
+//import sk.itsovy.
 
 public class Main {
+    private static final String ipadresPattern = "((2[0-5][0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\\.){3}(2[0-5][0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])[$0-9]";
+    private static final String slovakIdPattern = "([0-9]{2}[0156][0-9]{3}\\/?[0-9]{3,4})[^\\w]";
+    private static final String datePattern="^((([1-9]|([1-2][0-9]|31|30))\\.(1|3|5|7|9|11))|(([1-9]|([1-2][0-9]))\\.(2|4|6|8|10|12)))\\.((19|20)([0-9]{2}))$";
+    private static final String macAdressPattern="^([0-9A-Fa-z]{2}[:-]){5}([0-9A-Fa-z]{2})$";
+
     public static void main(String[] args) {
         DataIP dataIP = new DataIP();
+        PersonalID personalID = new PersonalID();
+        MacAddress macAddress=new MacAddress();
+        Date date = new Date();
 
         System.out.println(dataIP.get());
 
-        ipAdress(dataIP.get());
+        //findMatches(dataIP.get(),ipadresPattern);// works
+        //findMatches(personalID.get(),slovakIdPattern);
+        //findMatches(date.get(),datePattern);
+        findMatches(macAddress.get(),macAdressPattern);
+
+        ganoczi((ArrayList<String>) dataIP.get(),ipadresPattern);
+        ganoczi((ArrayList<String>) personalID.get(),slovakIdPattern);
+        ganoczi((ArrayList<String>) date.get(),datePattern);
+        ganoczi((ArrayList<String>) macAddress.get(),macAdressPattern);
 
     }
 
-    public static void slovakId(List list) {
-        String stringPattern = "[0-9]{2}[0156][0-9]{3}\\/?[0-9]{3,4}";
-
-        Pattern pattern = Pattern.compile(stringPattern);
-        //  Matcher matcher=new Matcher(list.get);
-
-
-    }
-
-    public static void ipAdress(List list) {
-        String stringPattern = "((2[0-5][0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\\.){3}(2[0-5][0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])[$0-9]";
+    public static void findMatches(List<String> list, String stringPattern) {
         String line = list.toString();
 
         Pattern pattern = Pattern.compile(stringPattern);
@@ -45,7 +55,20 @@ public class Main {
         }else {
             System.out.println("no matches");
         }
-        System.out.println(count);
+        System.out.println("Number of matches:"+count);
+    }
+    public static void ganoczi(ArrayList<String> list, String pattern){
+        Pattern pattern1=Pattern.compile(pattern);
 
+        for (int i=0; list.size()>i;i++) {
+
+            if (list.get(i) != null) {
+                Matcher matcher = pattern1.matcher(list.get(i));
+                while (matcher.find()) {
+                    System.out.println("Found match: " + matcher.group());
+                }
+            }
+        }
+        System.out.println();
     }
 }
